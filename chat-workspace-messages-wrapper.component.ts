a47a45bec5f0fa7c29a9f2d37ed15e077dc132cf
KOZLeraLOVA <valeriakozlova007@gmail.com>
2025-05-15 16:18:08 +0300

    Add files via upload

---
+++ chat-workspace-messages-wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {ChatWorkspaceMessageComponent} from './chat-workspace-message/chat-workspace-message.component';
+
+@Component({
+  selector: 'app-chat-workspace-messages-wrapper',
+  imports: [ChatWorkspaceMessageComponent],
+  templateUrl: './chat-workspace-messages-wrapper.component.html',
+  styleUrl: './chat-workspace-messages-wrapper.component.scss'
+})
+export class ChatWorkspaceMessagesWrapperComponent {
+
+}
